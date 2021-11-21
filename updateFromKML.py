9274dd347b274dd4f4293e6afd6d4b2af6b10d01
Jacob Helfman <jacobjhelfman@gmail.com>
2021-11-20 20:30:19 -0800

    Cleaning up

--- updateFromKML.py
+++ updateFromKML.py
@@ -5,6 +5,7 @@
 # 3. Here, the master_FC (variable pointing to feature class, "C:/HMPs Project/HMPs_Master.gdb/Wildlife_Points") matches what's in the "Wildlife Points" feature layer in AGOL
 # Notes: 
     #   Use correct Python interpreter (mine is "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe")
+    #   This where history is saved: C:\Users\User\AppData\Roaming\Esri\ArcGISPro\ArcToolbox\History
     #   Append projected_fc to feature service manually, b/c as of 11/19/2021, the feature_layer.append() method kept throwing this error:
         # Object reference not set to an instance of an object.
         # (Error Code: 400)
@@ -46,25 +47,26 @@ def removeExisting():
         # Remove FC if exists
         if arcpy.Exists(projected_fc):
             arcpy.management.Delete(projected_fc)
-            print(arcpy.GetMessages())
-            print(f"Completed removing existing {i}")
+            msg_Delete = f"Completed removing existing {i}" + arcpy.GetMessages()
+            arcpy.AddMessage(msg_Delete)
+            print(arcpy.GetMessages()) # for running in IDE
 
 
 # Convert KMZ file to Points FC (and layer)
 def kmlToFC():
     try:
-        # Remove stale data
-        removeExisting()
         # Convert KML/KMZ file
         arcpy.conversion.KMLToLayer(kmz_file_path, staging_folder) # https://pro.arcgis.com/en/pro-app/latest/tool-reference/conversion/kml-to-layer.htm
         # arcpy.AddMessage(arcpy.GetMessages())
-        print(f"Completed KML to Layer from {kmz_file_path} to {staging_folder}")
+        msg_KML = f"Completed KML to Layer from {kmz_file_path} to {staging_folder}"
+        arcpy.AddMessage(msg_KML)
         print(arcpy.GetMessages())
         # When indexing the result object or using its getOutput() method, the return value is a string.
         # print(kml_layer[0])
         # print(staging_fc)
         arcpy.management.Project(staging_kmzfc, projected_fc, spatial_ref, transform_method)
-        print(f"Completed Projecting {staging_kmzfc} to {projected_fc}, using {spatial_ref.name} and {transform_method}")
+        msg_Prj = f"Completed Projecting {staging_kmzfc} to {projected_fc}, using {spatial_ref.name} and {transform_method}"
+        arcpy.AddMessage(msg_Prj)
         print(arcpy.GetMessages())
           
     # Return geoprocessing specific errors
@@ -81,21 +83,26 @@ def kmlToFC():
 def appendTo():
     try:
         insert_cursor = arcpy.da.InsertCursor(master_FC, master_FC_fields+['SHAPE@'])
+        msg_Ins = f"Created Insert Cursor for {master_FC}"
+        arcpy.AddMessage(msg_Ins)
+        print(arcpy.GetMessages())
         
         count1 = arcpy.management.GetCount(master_FC)
-        print(f"{master_FC} is starting with a feature count of {count1}")
+        msg_Start = f"{master_FC} is starting with a feature count of {count1}"
+        arcpy.AddMessage(msg_Start)
+        print(arcpy.GetMessages())
         
         new_rows = 0
         with arcpy.da.SearchCursor(projected_fc, projected_fc_fields+['SHAPE@']) as cursor:
             for row in cursor:
                 insert_cursor.insertRow(row)
                 new_rows += 1
-
         del insert_cursor
         
-        print(f"Completed adding {new_rows} total features to {master_FC}")
         count2 = arcpy.management.GetCount(master_FC)
-        print(f"{master_FC} now has a feature count of {count2}")
+        msg_End1 = f"Completed adding {new_rows} total features to {master_FC}"
+        msg_End2 = f"{master_FC} now has a feature count of {count2}"
+        arcpy.AddMessage(msg_End1 + "\n" + msg_End2)
         print(arcpy.GetMessages())  
     # Return geoprocessing specific errors
     except arcpy.ExecuteError:    
@@ -111,37 +118,41 @@ def appendToFL():
         
         # 5.) Loop through Feature Services and Append to Wildlife Points layer 
         aprx = arcpy.mp.ArcGISProject("CURRENT")
-        arcpy.AddMessage(arcpy.GetMessages())  
-        m = aprx.listMaps("1. HMPs")[0] # index b/cx method returns a list
-        arcpy.AddMessage(arcpy.GetMessages())  
+        msg_aprx = f"Accessed the ArcGIS Pro project, {aprx}"
+        arcpy.AddMessage(msg_aprx)
+        print(arcpy.GetMessages())  
+        map_name = "1. HMPs"
+        m = aprx.listMaps(map_name)[0] # index b/cx method returns a list
+        msg_map = f"Accessed {map_name}map. Now looking through its layers!"
+        arcpy.AddMessage(msg_map)
+        print(arcpy.GetMessages())
         for lyr in m.listLayers():
-            arcpy.AddMessage(arcpy.GetMessages())  
-            if lyr.name == "Wildlife Points":         
+            msg_lyr = f"The {map_name} map has a layer named {lyr.name}"
+            arcpy.AddMessage(msg_lyr)
+            print(arcpy.GetMessages())  
+            if lyr.name == "Wildlife Points":  
                 insert_cursor = arcpy.da.InsertCursor(lyr, master_FC_fields+['SHAPE@'])
-                
-                arcpy.AddMessage(arcpy.GetMessages())  
+                msg_Inslyr = f"Created Insert Cursor for {lyr.name}"
+                arcpy.AddMessage(msg_Inslyr)
+                print(arcpy.GetMessages())
                 
                 count1 = arcpy.management.GetCount(lyr)
-                
-                arcpy.AddMessage(arcpy.GetMessages())  
-                
-                print(f"{lyr} is starting with a feature count of {count1}")
+                msg_count1 = f"{lyr} is starting with a feature count of {count1}"
+                arcpy.AddMessage(msg_count1)
+                print(arcpy.GetMessages())
                 
                 new_rows = 0
-                
                 with arcpy.da.SearchCursor(projected_fc, projected_fc_fields+['SHAPE@']) as cursor:
                     for row in cursor:
                         insert_cursor.insertRow(row)
                         new_rows += 1
-
                 del insert_cursor
                 
-                arcpy.AddMessage(arcpy.GetMessages())  
-                
-                print(f"Completed adding {new_rows} total features to {lyr}")
                 count2 = arcpy.management.GetCount(lyr)
-                print(f"{lyr} now has a feature count of {count2}")
-                arcpy.AddMessage(arcpy.GetMessages())  
+                msg_End1 = f"Completed adding {new_rows} total features to {lyr}"
+                msg_End2 = f"{lyr} now has a feature count of {count2}"
+                arcpy.AddMessage(msg_End1 + "\n" + msg_End2)
+                print(arcpy.GetMessages()) 
     # Return geoprocessing specific errors
     except arcpy.ExecuteError:    
         print(arcpy.GetMessages()) 
@@ -153,9 +164,9 @@ def appendToFL():
           
 
 if __name__ == "__main__":
-    # removeExisting()
-    # kmlToFC()
-    # appendTo()
+    removeExisting()
+    kmlToFC()
+    appendTo()
     appendToFL()
     
     
